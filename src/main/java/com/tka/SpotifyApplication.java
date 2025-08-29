08feed514b625f385250108bdb9f417904fc0db5
ankit-birgade27 <ankitbirgade@gmail.com>
2025-08-29 22:42:56 +0530

    initial commit

---
+++ src/main/java/com/tka/SpotifyApplication.java
@@ -0,0 +1,13 @@
+package com.tka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyApplication.class, args);
+        System.out.println("application started");
+    }
+}
+
